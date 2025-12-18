0c6db43fd0d5a2c7e95e92648df4ff988f94aed4
polinagapp <63647499+polinagapp@users.noreply.github.com>
2025-12-18 17:22:32 +1000

    КТ15
    КТ15

---
+++ WpfApp2/Window6.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window6.xaml
+    /// </summary>
+    public partial class Window6 : Window
+    {
+        //объект, который хранит ссылку на метод (или несколько методов)
+        //Объявление делегата
+        public delegate void TaskDelegate(string taskName);
+
+        //Использование встроенного делегата Action
+        private Action<string> showMessage;
+
+        //Использование делегата
+        private TaskDelegate taskAdded;
+
+        //Коллекция задач
+        private ObservableCollection<string> tasks = new ObservableCollection<string>();
+
+        public Window6()
+        {
+            InitializeComponent();
+            showMessage = msg => MessageBox.Show(msg);
+            taskAdded = name => Console.WriteLine($"Задача добавлена: {name}");
+            // Настройка ListBox
+            TasksListBox.ItemsSource = tasks;
+            AddButton.Click += AddButton_Click;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string taskName = TaskTextBox.Text;
+
+            if (!string.IsNullOrWhiteSpace(taskName))
+            {
+                //Вызов делегата Action
+                showMessage?.Invoke($"Добавлено: {taskName}");
+
+                //Вызов делегата
+                taskAdded?.Invoke(taskName);
+
+                //Добавление задачи
+                tasks.Add(taskName);
+                TaskTextBox.Text = "";
+            }
+        }
+    }
+}
